83725b1839999f646c813ec4e177d7868eb8d306
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-29 17:01:38 -0500

    .

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?php 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once __DIR__ . '/includes/app.php';
 
-require_once __DIR__ . '/../includes/app.php';
-
+use Controllers\InicioController;
 use MVC\Router;
+
+
 $router = new Router();
 
+echo "<h1>Hola Mundo</h1>";
+// $router->get('/', [InicioController::class, 'index']);
 
 
 
-// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
+
 $router->comprobarRutas();
