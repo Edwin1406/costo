32265506660b057b640420f63f0ed6afc76d7b3f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-29 16:48:23 -0500

    .

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,9 @@
 <?php 
-use Model\ActiveRecord;
-require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '/../includes/.env');
-$dotenv->safeLoad();
-
 
 require 'funciones.php';
 require 'database.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Conectarnos a la base de datos
+use Model\ActiveRecord;
 ActiveRecord::setDB($db);
--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,6 @@
 <?php
-$db = mysqli_connect(
-    $_ENV['DB_HOST'] ?? '',
-    $_ENV['DB_USER'] ?? '', 
-    $_ENV['DB_PASS'] ?? '', 
-    $_ENV['DB_NAME'] ?? ''
-);
+
+$db = mysqli_connect('localhost', '', '', '');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
